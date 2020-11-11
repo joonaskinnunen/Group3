aea3991ef3800575036dca9b9c16dbf1cd22922e
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-11-11 20:34:29 +0200

    Add authorization to routes

--- atm-backend-nodejs/controllers/cards.js
+++ atm-backend-nodejs/controllers/cards.js
@@ -1,105 +1,141 @@
 const cardsRouter = require('express').Router()
 const Card = require('../models/card')
+require('dotenv').config()
+
+const isAuthorized = (req) => {
+  const password = req.get('Password')
+  const username = req.get('Username')
+
+  if (process.env.PASSWORD == password && process.env.USERNAME == username) return true
+  return false
+}
 
 cardsRouter.get('/', async (request, response) => {
   const cards = await Card.find({})
-  response.json(cards.map(card => card.toJSON()))
+  if (isAuthorized(request)) {
+    response.json(cards.map(card => card.toJSON()))
+  }
+  response.status(400).send('Not authorized')
+
 })
 
 cardsRouter.get('/:id', async (request, response) => {
   const card = await Card.findById(request.params.id)
-  if (card) {
-    response.json(card.toJSON())
+  if (isAuthorized(request)) {
+    if (card) {
+      response.json(card.toJSON())
+    } else {
+      response.status(404).end()
+    }
   } else {
-    response.status(404).end()
+    response.status(400).send('Not authorized')
   }
 })
 
 cardsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const card = new Card({
-    cardId: body.cardId,
-    owner: body.owner,
-    pin: body.pin,
-    debitBalance: body.debitBalance,
-    creditBalance: 0,
-    creditLimit: body.creditLimit,
-    debitTransactions: [],
-    creditTransactions: []
-  })
+  if (isAuthorized(request)) {
+    const card = new Card({
+      cardId: body.cardId,
+      owner: body.owner,
+      pin: body.pin,
+      debitBalance: body.debitBalance,
+      creditBalance: 0,
+      creditLimit: body.creditLimit,
+      debitTransactions: [],
+      creditTransactions: []
+    })
 
-  const savedCard = await card.save()
-  response.json(savedCard.toJSON())
+    const savedCard = await card.save()
+    response.json(savedCard.toJSON())
+  }
+  response.status(400).send('Not authorized')
 })
-
-cardsRouter.put('/debitwithdrawal/:id', async (request, response, next) => {
+cardsRouter.put('/debitwithdrawal/:id', async (request, response) => {
   const body = request.body
 
-  const filter = { cardId: request.params.id };
-  const update = {$inc: { "debitBalance" : -body.amount }};
+  if (isAuthorized(request)) {
 
-  let card = await Card.findOneAndUpdate(filter, update, {
-    new: true
-  })
-  .then(updatedCard => {
-    response.json(updatedCard.toJSON())
-  })
-  .catch(error => next(error))
+    const filter = { cardId: request.params.id };
+    const update = { $inc: { "debitBalance": -body.amount } };
+
+    await Card.findOneAndUpdate(filter, update, {
+      new: true
+    })
+      .then(updatedCard => {
+        response.json(updatedCard.toJSON())
+      })
+      .catch(error => console.log(error))
+  } else {
+    response.status(400).send('Not authorized')
+  }
 })
 
-cardsRouter.put('/creditwithdrawal/:id', async (request, response, next) => {
+cardsRouter.put('/creditwithdrawal/:id', async (request, response) => {
   const body = request.body
 
-  const filter = { cardId: request.params.id };
-  const update = {$inc: { "creditBalance" : -body.amount }};
+  if (isAuthorized(request)) {
+    const filter = { cardId: request.params.id };
+    const update = { $inc: { "creditBalance": -body.amount } };
 
-  let card = await Card.findOneAndUpdate(filter, update, {
-    new: true
-  })
-  .then(updatedCard => {
-    response.json(updatedCard.toJSON())
-  })
-  .catch(error => next(error))
+    await Card.findOneAndUpdate(filter, update, {
+      new: true
+    })
+      .then(updatedCard => {
+        response.json(updatedCard.toJSON())
+      })
+      .catch(error => console.log(error))
+  }
+  response.status(400).send('Not authorized')
 })
 
-cardsRouter.put('/debitdeposit/:id', async (request, response, next) => {
+cardsRouter.put('/debitdeposit/:id', async (request, response) => {
   const body = request.body
 
-  const filter = { cardId: request.params.id };
-  const update = {$inc: { "debitBalance" : body.amount }};
+  if (isAuthorized(request)) {
+    const filter = { cardId: request.params.id };
+    const update = { $inc: { "debitBalance": body.amount } };
 
-  let card = await Card.findOneAndUpdate(filter, update, {
-    new: true
-  })
-  .then(updatedCard => {
-    response.json(updatedCard.toJSON())
-  })
-  .catch(error => next(error))
+    let card = await Card.findOneAndUpdate(filter, update, {
+      new: true
+    })
+      .then(updatedCard => {
+        response.json(updatedCard.toJSON())
+      })
+      .catch(error => console.log(error))
+  }
+  response.status(400).send('Not authorized')
 })
 
-cardsRouter.put('/creditdeposit/:id', async (request, response, next) => {
+cardsRouter.put('/creditdeposit/:id', async (request, response) => {
   const body = request.body
 
-  const filter = { cardId: request.params.id };
-  const update = {$inc: { "creditBalance" : body.amount }};
+  if (isAuthorized(request)) {
+    const filter = { cardId: request.params.id };
+    const update = { $inc: { "creditBalance": body.amount } };
 
-  let card = await Card.findOneAndUpdate(filter, update, {
-    new: true
-  })
-  .then(updatedCard => {
-    response.json(updatedCard.toJSON())
-  })
-  .catch(error => next(error))
+    await Card.findOneAndUpdate(filter, update, {
+      new: true
+    })
+      .then(updatedCard => {
+        response.json(updatedCard.toJSON())
+      })
+      .catch(error => console.log(error))
+  }
+  response.status(400).send('Not authorized')
 })
 
 cardsRouter.get('/:id', async (request, response) => {
   const card = await Card.findById(request.params.id)
-  if (card) {
-    response.json(card.toJSON())
-  } else {
-    response.status(404).end()
+  if (isAuthorized(request)) {
+    if (card) {
+      response.json(card.toJSON())
+    } else {
+      response.status(404).end()
+    }
   }
+  response.status(400).send('Not authorized')
 })
 
 module.exports = cardsRouter
--- atm-backend-nodejs/models/card.js
+++ atm-backend-nodejs/models/card.js
@@ -43,7 +43,6 @@ cardSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    delete returnedObject.passwordHash
   }
 })
 
