ebaeac462659447ad4909e86f235010124884dc8
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-11-11 18:46:28 +0200

    add withdrawal routes to cards controller

--- atm-backend-nodejs/controllers/cards.js
+++ atm-backend-nodejs/controllers/cards.js
@@ -6,6 +6,15 @@ cardsRouter.get('/', async (request, response) => {
   response.json(cards.map(card => card.toJSON()))
 })
 
+cardsRouter.get('/:id', async (request, response) => {
+  const card = await Card.findById(request.params.id)
+  if (card) {
+    response.json(card.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
 cardsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -22,6 +31,36 @@ cardsRouter.post('/', async (request, response) => {
 
   const savedCard = await card.save()
   response.json(savedCard.toJSON())
+})
+
+cardsRouter.put('/debitwithdrawal/:id', async (request, response, next) => {
+  const body = request.body
+
+  const filter = { cardId: request.params.id };
+  const update = {$inc: { "debitBalance" : -body.amount }};
+
+  let card = await Card.findOneAndUpdate(filter, update, {
+    new: true
+  })
+  .then(updatedCard => {
+    response.json(updatedCard.toJSON())
+  })
+  .catch(error => next(error))
+})
+
+cardsRouter.put('/creditwithdrawal/:id', async (request, response, next) => {
+  const body = request.body
+
+  const filter = { cardId: request.params.id };
+  const update = {$inc: { "creditBalance" : -body.amount }};
+
+  let card = await Card.findOneAndUpdate(filter, update, {
+    new: true
+  })
+  .then(updatedCard => {
+    response.json(updatedCard.toJSON())
+  })
+  .catch(error => next(error))
 })
 
 cardsRouter.get('/:id', async (request, response) => {
